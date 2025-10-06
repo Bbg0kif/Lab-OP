d1a827fd78f5f7106504ed4f7bf2b670c7a5f464
Oleksandra Beskrovna <b.oleksandra200@gmail.com>
2025-10-06 18:18:20 +0300

    Labs

---
+++ Lab3/1-random.js
@@ -0,0 +1,11 @@
+"use strict";
+const random = (min, max) => {
+  if (min === undefined) {
+    max = min;
+    min = 0;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+console.log(random(5, 10));
+console.log(random(11));
+module.exports = { random };
